19353ec9d2c4aa6d2b41bf896b3d5d54000bc1bd
Seliversto <seliverstovalexand91@gmail.com>
2020-09-07 00:34:47 +0300

    home

--- src/homework2/Input8.java
+++ src/homework2/Input8.java
@@ -1,24 +1,41 @@
 package homework2;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Input8 {
-    public static void main (String[] args) {
-        int [] cifr = new int [14];
-        cifr [0] = 1;
-        cifr [1] = -10;
-        cifr [2] = 5;
-        cifr [3] = 6;
-        cifr [4] = 45;
-        cifr [5] = 23;
-        cifr [6] = -45;
-        cifr [7] = -34;
-        cifr [8] = 0;
-        cifr [9] = 32;
-        cifr [10] = 56;
-        cifr [11] = -1;
-        cifr [12] = 2;
-        cifr [13] = -2;
-        System.out.println("Ваше число: " + cifr);
+    public static void main(String[] args) {
+        int[] cifr = {1, -10, 5, 6, 45, 23, -45, -34, 0, 32, 56, -1, 2, -2};
+        int max = cifr[0];
+        int pozsum = 0;
+        int otrizsum = 0;
+        int poznumb = 0;
+        int middlsum = 0;
+        int numbsum = 0;
+        for (int i = 0; i < cifr.length; i++) {
+            int abc = cifr[i];
+            if (max < abc) {
+                max = abc;
+            }
+            if (abc > 0) {
+                pozsum = pozsum + abc;
+            }
+            if (abc < 0 && abc % 2 == 0) {
+                otrizsum = otrizsum + abc;
+            }
+            if (abc > 0) {
+                poznumb = poznumb + 1;
+            }
+            if (abc < 0) {
+                numbsum = numbsum + 1;
+                middlsum = ((middlsum + abc) / numbsum);
+            }
+            System.out.println("Максимальное значение: " + max);
+            System.out.println("Сумма положительных чисел: " + pozsum);
+            System.out.println("Сумма отрицательных четных чисел: " + otrizsum);
+            System.out.println("Количество положительных чисел: " + poznumb);
+            System.out.println("Среднее арифметическое отрицательных чисел: " + middlsum );
+        }
     }
 }
+
--- src/homework2/Input9.java
+++ src/homework2/Input9.java
@@ -1,12 +1,26 @@
 package homework2;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Input9 {
-    public static void input1 (String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Введите число: ");
-        String numb = scan.next();
-        System.out.println("Ваше число: " + numb);
+    public static void main(String[] args) {
+        int[] cifr = {1, -10, 5, 6, 45, 23, -45, -34, 0, 32, 56, -1, 2, -2};
+        int[] cifr1 = {1, -10, 5, 6, 45, 23, -45, -34, 0, 32, 56, -1, 2, -2};
+        int max = cifr[0];
+        int pozsum = 0;
+        for (int i = 0; i < cifr.length; i++) {
+            int abc = cifr[i];
+            if (max < abc) {
+                max = abc;
+            }
+            if (abc > 0) {
+                cifr1[i] = 0;
+                pozsum = pozsum + abc;
+            }
+        }
+        System.out.println(max);
+        System.out.println(pozsum);
+        System.out.println(Arrays.toString(cifr1));
     }
 }
