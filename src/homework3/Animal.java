b129f863762dd56b3bea821993fa4d0621994923
Seliversto <seliverstovalexand91@gmail.com>
2020-09-09 22:25:28 +0300

    homework

--- src/homework3/Animal.java
+++ src/homework3/Animal.java
@@ -1,4 +1,15 @@
 package homework3;
 
 public class Animal {
+    public String food;
+    public String location;
+
+    public void makeNoise(String animal) {
+    }
+
+    public void eat(String animal) {
+    }
+    public void sleep(String animal) {
+        System.out.println( animal + " Спит");
+    }
 }
